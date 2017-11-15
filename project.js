a82c25b2966f404f2b6efda2dae00a1312d1e2d3
aesiely <aesiely@hawk.iit.edu>
2017-11-15 12:40:11 -0600

    map
    I put the map on my home page

--- project.js
+++ project.js
@@ -1,11 +1,19 @@
-function initMap() {
+/*function initMap() {
   var ras = {lat: 31.526038, lng: 31.843669};
   var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 4
-  center: ras
+  center: ras,
 });
   var marker = new google.maps.Marker({
   position: ras,
-  animation: google.maps.Animation.BOUNCE,
-  map: map
-});
+  animation: google.maps.Animation.drop,
+  map: map,
+  title: "Ras Elbar"
+});*/
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 31.526038, lng: 31.843669},
+          zoom: 8
+        });
+      }
