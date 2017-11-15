852a080eb9250b820933fae747fbb611a45104c5
aesiely <aesiely@hawk.iit.edu>
2017-11-14 23:21:40 -0600

    map in progress
    work in progress to upload my map to my webpage

---
+++ project.js
@@ -0,0 +1,11 @@
+function initMap() {
+  var ras = {lat: 31.526038, lng: 31.843669};
+  var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 4
+  center: ras
+});
+  var marker = new google.maps.Marker({
+  position: ras,
+  animation: google.maps.Animation.BOUNCE,
+  map: map
+});
