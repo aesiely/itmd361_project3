b849c16677d64fae76062f9dce99e812d645d8e0
aesiely <aesiely@hawk.iit.edu>
2017-11-15 23:42:31 -0600

    js file updated
    copied the js code from my html file to my js file

--- project.js
+++ project.js
@@ -1,17 +1,53 @@
-/*
-  function initMap(){
-  var theSpot = {lat: 31.526038, lng: 31.843669};
-  var map = new google.maps.Map(document.getElementById('map'), {
-  zoom = 10;
-  center: theSpot
-  });
-
-
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 31.526038, lng: 31.843669},
-          zoom: 15
-          mapTypeId: google.maps.MapTypeId.SATELLITE,
-        });
-      }*/
+function initMap() {
+  //Map options
+  var options = {
+    zoom: 5,
+    center: {lat: 26.8206, lng: 30.8025},
+    mapTypeControlOptions: {
+    position: google.maps.ControlPosition.BOTTOM_CENTER
+    }
+   }
+   
+  //New map
+    var map = new google.maps.Map(document.getElementById('map'), options);
+    addMarker({
+     coords: {lat:31.526038, lng:31.843669},
+     iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+     content: '<h1>Hang out spot in my home town, Ras El Bar</h1>'
+     });
+    addMarker({
+     coords: {lat:30.0444, lng:31.2357},    
+     content: '<h1>The capital of Egypt, Cairo</h1>'
+     });
+    addMarker({
+     coords: {lat:27.2579 , lng:33.8116 },
+     iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+     content: '<h1>A beautiful get away for a vacation, Hurghada</h1>'
+     });
+       
+  //Add Marker function
+    function addMarker(props){
+    var marker = new google.maps.Marker({
+     position: props.coords,
+     map: map,
+     icon: props.iconImage             
+     });
+              
+   //check for customIcon
+    if(props.iconImage){
+   //set icon image
+     marker.setIcon(props.iconImage);
+     } 
+              
+   //check content
+    if (props.content) {
+    var infoWindow = new google.maps.InfoWindow({
+     content: props.content
+    });
+                
+     marker.addListener('click', function(){
+       infoWindow.open(map, marker);
+    });
+      }
+     }
+    }       
