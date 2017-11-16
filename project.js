49df5286f59ac1b589e37dff7a26d6596ccb7bc7
aesiely <aesiely@hawk.iit.edu>
2017-11-15 21:25:14 -0600

    map
    demo in the html to check if it works then going to be moved to js file

--- project.js
+++ project.js
@@ -1,21 +1,17 @@
-/*function initMap() {
-  var ras = {lat: 31.526038, lng: 31.843669};
+/*
+  function initMap(){
+  var theSpot = {lat: 31.526038, lng: 31.843669};
   var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 4
-  center: ras,
-});
-  var marker = new google.maps.Marker({
-  position: ras,
-  animation: google.maps.Animation.drop,
-  map: map,
-  title: "Ras Elbar"
-});*/
+  zoom = 10;
+  center: theSpot
+  });
+
+
 var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 31.526038, lng: 31.843669},
+          zoom: 15
           mapTypeId: google.maps.MapTypeId.SATELLITE,
-          zoom: 8
-          
         });
-      }
+      }*/
